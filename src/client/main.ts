3cb2e7964e139033212320a55a9e31c1e90dab89
Perry Robinson <perryrobinson68@gmail.com>
2026-02-05 15:48:45 -0600

    Fix empty state button alignment
    Remove span wrapper around button text to match toolbar button structure.
    The span was causing icon/text misalignment.

--- src/client/main.ts
+++ src/client/main.ts
@@ -241,13 +241,13 @@ function showEmptyState() {
             <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
             <polyline points="14 2 14 8 20 8"/>
           </svg>
-          <span>Open File</span>
+          Open File
         </button>
         <button class="btn" id="empty-open-folder">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/>
           </svg>
-          <span>Open Folder</span>
+          Open Folder
         </button>
       </div>
     </div>
